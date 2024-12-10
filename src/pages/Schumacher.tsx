798c0ffcc8cafb909a545cc67dcb175b83f5ca78
tanisha <tanisha@seasia.in>
2024-12-10 11:21:09 +0530

    schumacher-done-for large-screen

--- src/pages/Schumacher.tsx
+++ src/pages/Schumacher.tsx
@@ -10,6 +10,9 @@ import Newschumacher from "../assets/new-schumacher-mobile.png";
 import Problemtwobanner from "../assets/problem2-banner1.png";
 import Problemtwobannerabsolute from "../assets/problem2-banner2.png";
 import AddressedIssue from "../assets/addressedissue-banner.png";
+import ProblemthreeBanner from "../assets/problem3-banner.png";
+import ProblemthreeBannerAbsolute from "../assets/problem3-absolute.png";
+import SolutionthreeBanner from "../assets/solution3-banner.png";
 import checkedLinesBg from "../assets/checked-lines-bg.png";
 import DrivenDesign from "../components/Homepage/DrivenDesign";
 import Features from "../components/Homepage/Features";
@@ -257,9 +260,9 @@ const Schumacher = () => {
                 </Container>
             </section>
 
-            <section className="bg-secondary-dark2">
+            <section className="problem2-section">
                 <Container>
-                    <div className="grid grid-cols-2 gap-20  items-end pb-[140px] mt-[100px]">
+                    <div className="grid grid-cols-2 gap-20  items-end justify-content-center pt-[150px] pb-[300px] mt-[100px]  border-dashed border-b-2 border-[rgba(255,255,255,0.20)] text-white ">
                         <div className="col-span-1 self-start">
                             <div>
 
@@ -282,28 +285,103 @@ const Schumacher = () => {
                             </div>
                         </div>
                     </div>
-                    <div className="grid grid-cols-2 gap-20  items-end pb-[140px] mt-[100px]">
-                        <div className="col-span-1 self-start">
-                            <div>
+                    <div className="grid grid-cols-12 gap-20  items-center py-[130px]  ">
+                        <div className="col-span-1"></div>
 
-
+                        <div className="col-span-6">
+                            <div className="address-content-wrapper w-100 text-white ">
                                 <div className="text-[40px] font-semibold mt-4 mb-8">
                                     How we addressed this issue.                                </div>
                                 <p className="text-xl font-medium">
                                     After reviewing Schumacher's website navigation and layout, we found it caused significant confusion among users. To tackle this issue, we analyzed real user data research to understand the extent of the problem. Based on what we learned, we provided recommendations to the client and then developed wireframes and UI designs that follow the best practices in user-friendly design.
                                 </p>
+                            </div>
+                        </div>
+                        <div className="col-span-4">
+                            <div className="relative">
+                                <img src={AddressedIssue} alt="image" />
+                            </div>
+                        </div>
+                        <div className="col-span-1"></div>
+
+                    </div>
+                </Container>
+            </section>
+
+            <section className="pt-[140px]">
+                <Container>
+
+
+                    <div className="grid grid-cols-2 gap-20  items-end pb-[140px]">
+                        <div className="col-span-1 self-start">
+                            <div>
+
+
+
+                                <div className="text-[18px] font-semibold text-secondary-light2">
+                                    PROBLEM 3
+                                </div>
+                                <div className="text-[40px] font-semibold mt-4 mb-8">
+                                    Enhancing the Process Screen with Detailed Information
+                                </div>
+
+
+
+
+                                <p className="text-xl font-medium">
+
+                                    The client requested a redesign of the Process screen to incorporate more detailed information. The aim was to provide users with a clearer and more comprehensive understanding of the procedures involved. By expanding the content and improving the layout, the redesigned screen will offer a more informative and user-friendly experience, ensuring that all necessary details are easily accessible and clearly presented.
+                                </p>
+
 
                             </div>
                         </div>
                         <div className="col-span-1">
                             <div className="relative">
-                                <img src={AddressedIssue} alt="image" />
+                                <img src={ProblemthreeBanner} alt="image" />
+                                <img src={ProblemthreeBannerAbsolute} alt="image" className="absolute top-[60%] right-[40%]" />
+                            </div>
+                        </div>
+                    </div>
+
+
+                    <div className="grid grid-cols-12 gap-0 mt-[100px] items-center py-[140px]">
+                        <div className="col-span-12">
+                            <div className="text-[40px] font-semibold mt-4 mb-8 text-center">
+                                Our approach to solving this problem.
+                            </div>
+                        </div>
+
+                        <div className="col-span-5">
+                            <div className="flex items-center justify-end">
+                                <img src={SolutionthreeBanner} alt="image" />
+                            </div>
+                        </div>
+                        <div className="col-span-1">
+                            <div className="flex items-center justify-center">
+                                <img src={Midconnector} alt="image" />
+                            </div>
+                        </div>
+                        <div className="col-span-6 ">
+                            <div className="bg-secondary-dark2 rounded-3xl text-xl font-medium text-white mx-auto">
+                                <div className="grid grid-cols-4 items-start gap-4 p-[50px]">
+                                    <div className="col-span-2">
+                                        <p >
+                                            We conducted a thorough competitor analysis of the Process screen using the websites provided by the client. This analysis allowed us to identify best practices and effective design elements used by competitors. Based on these insights, we formulated detailed recommendations for enhancing the client's Process screen.
+                                        </p>
+                                    </div>
+                                    <div className="col-span-2">
+                                        <p >
+                                            After presenting and discussing these recommendations with the client, we moved forward with developing wireframes to map out the improved structure. Finally, we created comprehensive UI designs that incorporated our findings, ensuring a more intuitive and user-friendly experience for visitors.
+                                        </p>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </Container>
             </section>
-
+            <Footer />
 
         </main>
     );
