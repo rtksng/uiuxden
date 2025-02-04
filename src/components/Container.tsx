916053181afc27e872801b7dab1775bd5a4c4017
rtksng <ritik@uiuxden.com>
2025-02-04 11:56:47 +0530

    uifixe

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -7,7 +7,7 @@ interface ContainerProps {
 
 const Container: React.FC<ContainerProps> = ({ children, className = "" }) => {
   return (
-    <div className={`max-w-screen-1600 mx-auto px-4 sm:px-6 lg:px-8 ${className}`}>
+    <div className={`max-w-screen-1600 mx-auto px-4 sm:px-6 lg:px-8 xl:px-20 ${className}`}>
       {children}
     </div>
   );
