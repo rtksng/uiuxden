7a6deced098e8f42559c4ae1e7eec04266101f8a
tanisha <tanisha@seasia.in>
2025-03-17 11:21:25 +0530

    blog-detail-responsive

--- src/pages/BlogDetail.tsx
+++ src/pages/BlogDetail.tsx
@@ -41,7 +41,7 @@ const BlogDetail = () => {
             <section className="relative pt-12 ">
                 <div className="blog-detail-header mt-6">
                     <Container>
-                        <div className="pt-[80px] sm:pt-[100px] xl:pt-[90px] flex flex-col items-center">
+                        <div className="pt-[40px] sm:pt-[50px] xl:pt-[90px] flex flex-col items-center">
                             <h1 className="text-[34px] sm:text-[48px] lg:text-[46px] xl:text-[60px] text-black  font-semibold mb-base ">
                                 Top AI Online Courses for UI/UX Designers: Boost Your Career
                             </h1>
@@ -52,9 +52,9 @@ const BlogDetail = () => {
             <section className="top-blog py-10 sm:py-8 xl:pt-9 xl:pb-[120px]">
                 <Container>
                     <div>
-                        <div className="grid grid-cols-12 gap-[50px]">
-                            <div className="col-span-12 xl:col-span-8">
-                                <div className="flex items-center justify-between mb-10">
+                        <div className="grid grid-cols-12 gap-8 xl:gap-[50px]">
+                            <div className="col-span-12 lg:col-span-7 xl:col-span-8">
+                                <div className="flex flex-col sm:flex-row sm:items-center justify-between mb-6 sm:mb-10">
                                     <div className="flex items-center">
                                         <div className="flex items-center border-r-2 border-[#EDEDED]  pr-3">
                                             <div className="border border-[#CDCDCD] border-[1px] w-[48px] h-[48px] mr-3 overflow-hidden rounded-full">
@@ -71,7 +71,7 @@ const BlogDetail = () => {
                                             </p>
                                         </div>
                                     </div>
-                                    <div className="flex items-center">
+                                    <div className="flex items-center mt-3 sm:mt-0 justify-end sm:justify-start">
                                         <IoEyeOutline className="text-[#67D17E] mr-2 text-xl" />
                                         <p className="text-[20px] text-primary-dark2 font-medium">
                                             207
@@ -83,17 +83,17 @@ const BlogDetail = () => {
                                 <div className="card-img-clip h-[250px] sm:h-[550px] w-full rounded-[18px] overflow-hidden">
                                     <img src={CardImg} alt="blogcard" className="w-full h-full object-cover" />
                                 </div>
-                                <div className="my-11 border-b-[1px] border-[#CDCDCDAD] pb-20 pl-8">
+                                <div className="my-6 sm:my-11 border-b-[1px] border-[#CDCDCDAD] pb-10 sm:pb-20 xl:pl-8">
                                     <div>
-                                        <h3 className="text-[40px] text-primary-dark2 font-semibold mb-8">
+                                        <h3 className="text-[40px] text-primary-dark2 font-semibold mb-4 sm:mb-8">
                                             Introduction
                                         </h3>
-                                        <p className="text-[20px] text-primary-dark2 font-normal mb-12">
+                                        <p className="text-[20px] text-primary-dark2 font-normal mb-8 sm:mb-12">
                                             Artificial intelligence (AI) is revolutionizing many industries, and design is no exception. As AI technology becomes more advanced, it is increasingly integrated into the workflows of UI and UX designers. Having a strong understanding of AI is no longer just a bonus but a necessity for designers who want to stay ahead of the curve in 2025.
                                         </p>
                                     </div>
                                     <div>
-                                        <h3 className="text-[40px] text-primary-dark2 font-semibold mb-8">
+                                        <h3 className="text-[40px] text-primary-dark2 font-semibold mb-4 sm:mb-8">
                                             Why Designers Should Learn AI in 2025
                                         </h3>
                                         <h5 className="text-[30px] text-primary-dark2 font-semibold mb-5">
@@ -110,7 +110,7 @@ const BlogDetail = () => {
                                         <p className="text-[20px] text-primary-dark2 font-normal mb-8">
                                             Artificial intelligence has fundamentally changed how designers approach their work.
                                         </p>
-                                        <ul className="list-disc list-outside pl-20">
+                                        <ul className="list-disc list-outside pl-7 sm:pl-20">
                                             <li className="text-[20px] text-primary-dark2 font-normal mb-4">
                                                 <span className="font-semibold">
                                                     Enhanced Creativity:
@@ -138,8 +138,8 @@ const BlogDetail = () => {
                                         </ul>
                                     </div>
                                 </div>
-                                <div className="bg-[#F3F6F8AD] rounded-[18px] p-8 xl:p-[60px]">
-                                    <div className="flex gap-6">
+                                <div className="bg-[#F3F6F8AD] rounded-[18px] p-5 sm:p-8 xl:p-[60px]">
+                                    <div className="flex flex-col sm:flex-row gap-4 sm:gap-6">
                                         <div className="border border-[#CDCDCD] border-[2px] min-w-[100px] w-[100px] h-[100px] overflow-hidden rounded-full">
                                             <img src={CardImg} alt="" className="w-full h-full object-cover" />
                                         </div>
@@ -159,19 +159,18 @@ const BlogDetail = () => {
                                     </div>
                                 </div>
                                 <div>
-                                    <div className="flex  items-center gap-4 mt-[100px] mb-7">
+                                    <div className="flex  items-center gap-4 mt-11 sm:mt-[100px] mb-7">
                                         <h3 className="text-[40px] font-semibold text-primary-dark2">
                                             Comments
                                         </h3>
                                         <div className="h-[1px] bg-[#CCCCCCAD] grow">
-
                                         </div>
                                     </div>
-                                    <div className="bg-[#F3F6F8AD] rounded-[18px] p-8 xl:p-[60px]">
-                                        <div className="grid grid-cols-12 gap-[50px]">
+                                    <div className="bg-[#F3F6F8AD] rounded-[18px] p-5 sm:p-8 xl:p-[60px]">
+                                        <div className="grid grid-cols-12 gap-4 sm:gap-[50px]">
                                             <div className="col-span-12 xl:col-span-6">
                                                 <div>
-                                                    <label className="text-[20px] font-medium text-primary-dark2     ">
+                                                    <label className=" text-[18px] sm:text-[20px] font-medium text-primary-dark2     ">
                                                         First Name
                                                     </label>
                                                     <div className="flex items-center bg-white px-4 outline-1 -outline-offset-1 border border-[#CBCBCB] has-[input:focus-within]:outline-2 has-[input:focus-within]:-outline-offset-2 has-[input:focus-within]:outline-indigo-600  rounded-[12px] mt-2">
@@ -180,7 +179,7 @@ const BlogDetail = () => {
                                                             name="price"
                                                             type="text"
                                                             placeholder="Enter your first name"
-                                                            className="block min-w-0 grow py-4 text-base text-gray-900 placeholder:text-secondary-dark focus:outline-none sm:text-[18px]"
+                                                            className="block min-w-0 grow py-3 sm:py-4 text-base text-gray-900 placeholder:text-secondary-dark focus:outline-none sm:text-[18px]"
                                                         />
                                                         <div className="shrink-0 text-base text-gray-500 select-none sm:text-sm/6"><RiUser3Line className="text-primary-dark2 text-xl" />
                                                         </div>
@@ -190,7 +189,7 @@ const BlogDetail = () => {
                                             </div>
                                             <div className="col-span-12 xl:col-span-6">
                                                 <div>
-                                                    <label className="text-[20px] font-medium text-primary-dark2     ">
+                                                    <label className=" text-[18px] sm:text-[20px] font-medium text-primary-dark2     ">
                                                         Email Id
                                                     </label>
                                                     <div className="flex items-center bg-white px-4 outline-1 -outline-offset-1 border border-[#CBCBCB] has-[input:focus-within]:outline-2 has-[input:focus-within]:-outline-offset-2 has-[input:focus-within]:outline-indigo-600  rounded-[12px] mt-2">
@@ -199,7 +198,7 @@ const BlogDetail = () => {
                                                             name="price"
                                                             type="text"
                                                             placeholder="Enter you email"
-                                                            className="block min-w-0 grow py-4 text-base text-gray-900 placeholder:text-secondary-dark focus:outline-none sm:text-[18px]"
+                                                            className="block min-w-0 grow py-3 sm:py-4 text-base text-gray-900 placeholder:text-secondary-dark focus:outline-none sm:text-[18px]"
                                                         />
                                                         <div className="shrink-0 text-base text-gray-500 select-none sm:text-sm/6"><RiUser3Line className="text-primary-dark2 text-xl" />
                                                         </div>
@@ -209,17 +208,17 @@ const BlogDetail = () => {
                                             </div>
                                             <div className="col-span-12 xl:col-span-12">
                                                 <div>
-                                                    <label className="text-[20px] font-medium text-primary-dark2     ">
+                                                    <label className=" text-[18px] sm:text-[20px] font-medium text-primary-dark2     ">
                                                         Comments
                                                     </label>
                                                     <textarea
                                                         id="price"
                                                         name="price"
                                                         rows={5}
-                                                        className="block min-w-0 grow py-4 text-base text-gray-900 placeholder:text-secondary-dark focus:outline-none sm:text-[18px] border border-[#CBCBCB] rounded-[12px] px-4 w-full mt-2"
+                                                        className="block min-w-0 grow py-3 sm:py-4 text-base text-gray-900 placeholder:text-secondary-dark focus:outline-none sm:text-[18px] border border-[#CBCBCB] rounded-[12px] px-4 w-full mt-2"
                                                     />
                                                 </div>
-                                                <div className="flex justify-start items-center mt-12">
+                                                <div className="flex justify-start items-center mt-7 sm:mt-12">
                                                     <Button
                                                         variant="primary"
                                                         className="!hover:bg-transparent py-[10px] 2xl:py-4 px-4 xl:px-[50px]"
@@ -232,7 +231,7 @@ const BlogDetail = () => {
                                     </div>
                                 </div>
                             </div>
-                            <div className="col-span-12 xl:col-span-4">
+                            <div className="col-span-12 lg:col-span-5 xl:col-span-4">
                                 <div>
                                     <div className="flex  items-center gap-4 mb-8">
                                         <h3 className="text-[30px] font-semibold text-primary-dark2">
@@ -242,13 +241,13 @@ const BlogDetail = () => {
                                         </div>
                                     </div>
                                     <div className="xl:h-full blog-detail-related-card-bg p-4">
-                                        <div className="grid grid-cols-12 gap-7  ">
-                                            <div className="col-span-12 xl:col-span-5">
-                                                <div className="w-full h-[150px]  rounded-[12px] overflow-hidden">
+                                        <div className="grid grid-cols-12 gap-4 xl:gap-7  ">
+                                            <div className="col-span-5 sm:col-span-4 xl:col-span-5">
+                                                <div className="w-full h-full  rounded-[12px] overflow-hidden">
                                                     <img src={CardImg} alt="blogcard" className="w-full h-full object-cover" />
                                                 </div>
                                             </div>
-                                            <div className="col-span-12 xl:col-span-7">
+                                            <div className="col-span-7 sm:col-span-8 xl:col-span-7">
 
                                                 <div className="flex justify-start items-center items-center">
                                                     <FaRegClock className="text-[#67D17E]" />
@@ -257,10 +256,10 @@ const BlogDetail = () => {
                                                     </p>
                                                 </div>
 
-                                                <h4 className="text-[18px] font-semibold text-primary-dark2 line-clamp-2 mt-3 mb-5">
-                                                    Top Online UX Design Courses for Career Changer: Get
+                                                <h4 className="text-[18px] font-semibold text-primary-dark2 line-clamp-2 sm:line-clamp-3 lg:line-clamp-2 mt-5 xl:mt-3 mb-5">
+                                                    Top Online UX Design Courses for Career Changer: Get Top Online UX Design Course Top Online UX Design Course Top Online UX Design Course
                                                 </h4>
-                                                <div className="flex justify-start items-center">
+                                                <div className="flex justify-start items-center pb-5 ">
                                                     <Button
                                                         variant="primary"
                                                         className="!hover:bg-transparent py-[10px] 2xl:py-2 px-4 xl:px-[16px]"
