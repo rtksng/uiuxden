a4a81ff0c96ee9becce5f035740668344a562582
tanisha <tanisha@seasia.in>
2025-04-09 15:27:49 +0530

    navbar-dropdown-responsive

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,67 +1,232 @@
-
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { motion, AnimatePresence } from "framer-motion";
 import { Link } from "react-router-dom";
 import { CiSearch } from "react-icons/ci";
 import { IoClose } from "react-icons/io5";
+import { CgMenuRightAlt } from "react-icons/cg";
 import Container from "./Container";
 import Button from "./Button";
 import logo from "../assets/logo.svg";
-import { CgMenuRightAlt } from "react-icons/cg";
+import Icon from "../assets/icons/Icons";
 
 const NavLinks: React.FC<{ isMobile?: boolean; onClick?: () => void }> = ({
   isMobile,
   onClick,
-}) => (
-  <motion.ul
-    className={`${isMobile
-      ? "flex flex-col items-start px-8 py-2 space-y-10 bg-secondary-light2 text-white h-[60%] sm:h-full justify-center"
-      : "hidden xl:flex items-center space-x-6"
-      } font-semibold xl:font-normal`}
-    initial={{ opacity: 0, x: isMobile ? -100 : 0 }}
-    animate={{ opacity: 1, x: 0 }}
-    exit={{ opacity: 0, x: isMobile ? -100 : 0 }}
-    transition={{ duration: 0.3 }}
-  >
-    {["UX Services +", "Portfolio +", "Blogs", "Contact", "Our Store"].map(
-      (link, idx) => (
-        <li
-          key={idx}
-          className="hover:text-black text-primary-dark hover:font-semibold font-medium transition-all text-2xl xl:text-base whitespace-nowrap"
-          onClick={onClick}
-        >
-          {idx === 4 ? (
-            <a
-              href="https://www.uiuxden.com/"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="text-inherit"
-            >
-              {link}
-            </a>
-          ) : (
-            <Link
-              onClick={() => {
-                window.scrollTo(0, 0);
-              }}
-              to={
-                idx === 1
-                  ? "/portfolio"
-                  : idx === 2
-                    ? "/blog-list"
-                    : idx === 3
-                      ? "/contact-us"
-                      : "/"
+}) => {
+  const [firstLinkActive, setFirstLinkActive] = useState(false);
+  const dropdownRef = useRef<HTMLLIElement>(null);
+
+  const handleClickOutside = (event: MouseEvent) => {
+    if (
+      dropdownRef.current &&
+      !dropdownRef.current.contains(event.target as Node)
+    ) {
+      setFirstLinkActive(false);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
+  return (
+    <motion.ul
+      className={`${isMobile
+        ? "flex flex-col items-start px-8 py-6 xl:py-2 space-y-10 bg-white text-white h-auto xl:h-full justify-center"
+        : "hidden xl:flex items-center space-x-6"
+        } font-semibold xl:font-normal`}
+      initial={{ opacity: 0, x: isMobile ? -100 : 0 }}
+      animate={{ opacity: 1, x: 0 }}
+      exit={{ opacity: 0, x: isMobile ? -100 : 0 }}
+      transition={{ duration: 0.3 }}
+    >
+      {["UX Services", "Portfolio", "Blogs", "Contact", "Our Store"].map(
+        (link, idx) => (
+          <li
+            key={idx}
+            ref={idx === 0 ? dropdownRef : null}
+            className={`hover:text-black text-primary-dark font-medium transition-all text-2xl xl:text-base whitespace-nowrap services-link 
+              ${idx === 0
+                ? firstLinkActive
+                  ? "relative minus pe-5"
+                  : "relative plus pe-5"
+                : ""
+              }`}
+            onClick={() => {
+              if (idx === 0) {
+                setFirstLinkActive((prev) => !prev);
+              } else {
+                onClick?.(); // close menu for all links except the first one
               }
-            >
-              {link}
-            </Link>
-          )}
-        </li>
-      )
-    )}
-  </motion.ul>
-);
+            }}
+          >
+            {idx === 0 ? (
+              <button
+                type="button"
+                className="relative"
+              >
+                {link}
+                <div
+                  className={` bg-[#f9f9ff] xl:bg-[#fff] rounded-[10px] left-0 sm:left-[150px] xl:sm:left-[50px] top-[30px] sm:top-[0px] xl:top-[60px] w-[300px] sm:w-[475px] xl:w-[750px] 2xl:w-[1000px] shadow-2xl  transition-transform duration-500 ease-in-out transform absolute ${idx === 0 && firstLinkActive ? " p-4 sm:p-9 translate-y-0 border border-[1px] border-[#EDEDED] " : "h-[0] -translate-y-10 overflow-hidden p-0 border-0"
+                    }`}
+                >
+                  <div className="grid grid-cols-12 gap-4 sm:gap-6">
+
+                    <div className=" sm:col-span-6 xl:col-span-4 col-span-12 flex flex-col gap-4 sm:gap-[36px]">
+                      <Link to="/humanization" className="flex gap-[10px] group">
+                        <span>
+                          <Icon
+                            name="humani"
+                            color="#393939"
+                            className="group-hover:text-[#3ABF88] transition-colors duration-300"
+                          />
+                        </span>
+                        <p className="text-[16px] color-primary-dark font-normal group-hover:text-[#3ABF88] transition-colors duration-300">
+                          Humanization
+                        </p>
+                      </Link>
+                      <Link to="/ui-design" className="flex gap-[10px] group">
+                        <span>
+                          <Icon
+                            name="uidesign"
+                            color="#393939"
+                            className="group-hover:text-[#3ABF88] transition-colors duration-300"
+                          />
+                        </span>
+                        <p className="text-[16px] color-primary-dark font-normal group-hover:text-[#3ABF88] transition-colors duration-300">
+                          UI Design
+                        </p>
+                      </Link>
+                      <Link to="/ux-strategy" className="flex gap-[10px] group">
+                        <span>
+                          <Icon
+                            name="uistrategy"
+                            color="#393939"
+                            className="group-hover:text-[#3ABF88] transition-colors duration-300"
+                          />
+                        </span>
+                        <p className="text-[16px] color-primary-dark font-normal group-hover:text-[#3ABF88] transition-colors duration-300">
+                          UX Strategy
+                        </p>
+                      </Link>
+                    </div>
+
+                    <div className=" sm:col-span-6 xl:col-span-4 col-span-12 flex flex-col gap-4 sm:gap-[36px]">
+                      <Link to="/ui-branding" className="flex gap-[10px] group">
+                        <span>
+                          <Icon
+                            name="productbranding"
+                            color="#393939"
+                            className="group-hover:text-[#3ABF88] transition-colors duration-300"
+                          />
+                        </span>
+                        <p className="text-[16px] color-primary-dark font-normal group-hover:text-[#3ABF88] transition-colors duration-300">
+                          Product Branding
+                        </p>
+                      </Link>
+                      <Link to="/sustainable" className="flex gap-[10px] group">
+                        <span>
+                          <Icon
+                            name="sustainable"
+                            color="#393939"
+                            className="group-hover:text-[#3ABF88] transition-colors duration-300"
+                          />
+                        </span>
+                        <p className="text-[16px] color-primary-dark font-normal group-hover:text-[#3ABF88] transition-colors duration-300">
+                          Sustainable UI/UX
+                        </p>
+                      </Link>
+                      <Link to="/design-sprint" className="flex gap-[10px] group">
+                        <span>
+                          <Icon
+                            name="designsprint"
+                            color="#393939"
+                            className="group-hover:text-[#3ABF88] transition-colors duration-300"
+                          />
+                        </span>
+                        <p className="text-[16px] color-primary-dark font-normal group-hover:text-[#3ABF88] transition-colors duration-300">
+                          Design Sprints
+                        </p>
+                      </Link>
+                    </div>
+
+                    <div className=" sm:col-span-6 xl:col-span-4 col-span-12 flex flex-col gap-4 sm:gap-[36px]">
+                      <Link to="/ux-audit" className="flex gap-[10px] group">
+                        <span>
+                          <Icon
+                            name="uxaudit"
+                            color="#393939"
+                            className="group-hover:text-[#3ABF88] transition-colors duration-300"
+                          />
+                        </span>
+                        <p className="text-[16px] color-primary-dark font-normal group-hover:text-[#3ABF88] transition-colors duration-300">
+                          Product UX Audit
+                        </p>
+                      </Link>
+                      <Link to="/ux-consultancy" className="flex gap-[10px] group">
+                        <span>
+                          <Icon
+                            name="uxconsult"
+                            color="#393939"
+                            className="group-hover:text-[#3ABF88] transition-colors duration-300"
+                          />
+                        </span>
+                        <p className="text-[16px] color-primary-dark font-normal group-hover:text-[#3ABF88] transition-colors duration-300">
+                          UX Consultancy
+                        </p>
+                      </Link>
+                      <Link to="/accessiblity" className="flex gap-[10px] group">
+                        <span>
+                          <Icon
+                            name="accessiblity"
+                            color="#393939"
+                            className="group-hover:text-[#3ABF88] transition-colors duration-300"
+                          />
+                        </span>
+                        <p className="text-[16px] color-primary-dark font-normal group-hover:text-[#3ABF88] transition-colors duration-300">
+                          Accessibility
+                        </p>
+                      </Link>
+                    </div>
+                  </div>
+                </div>
+              </button>
+            ) : idx === 4 ? (
+              <a
+                href="https://www.uiuxden.com/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-inherit"
+              >
+                {link}
+              </a>
+            ) : (
+              <Link
+                onClick={() => window.scrollTo(0, 0)}
+                to={
+                  idx === 1
+                    ? "/portfolio"
+                    : idx === 2
+                      ? "/blog-list"
+                      : idx === 3
+                        ? "/contact-us"
+                        : "/"
+                }
+              >
+                {link}
+              </Link>
+            )}
+
+
+          </li>
+        )
+      )}
+    </motion.ul>
+  );
+};
 
 const Navbar: React.FC = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -70,11 +235,7 @@ const Navbar: React.FC = () => {
 
   const toggleMenu = () => {
     setIsMenuOpen((prev) => !prev);
-    if (!isMenuOpen) {
-      document.body.style.overflow = 'hidden';
-    } else {
-      document.body.style.overflow = 'unset';
-    }
+    document.body.style.overflow = !isMenuOpen ? "hidden" : "unset";
   };
 
   useEffect(() => {
@@ -85,7 +246,7 @@ const Navbar: React.FC = () => {
     window.addEventListener("scroll", handleScroll);
     return () => {
       window.removeEventListener("scroll", handleScroll);
-      document.body.style.overflow = 'unset';
+      document.body.style.overflow = "unset";
     };
   }, []);
 
@@ -96,7 +257,6 @@ const Navbar: React.FC = () => {
     >
       <Container>
         <nav className="flex items-center justify-between py-3 xl:py-2 relative z-50">
-          {/* Logo */}
           <Link to="/" className="z-50">
             <img
               src={logo}
@@ -105,7 +265,6 @@ const Navbar: React.FC = () => {
             />
           </Link>
 
-          {/* Mobile Menu Toggle */}
           <button
             className="xl:hidden w-10 h-10 flex items-center justify-center rounded-full text-gray-900 hover:bg-gray-100 z-50 transition-colors"
             onClick={toggleMenu}
@@ -114,7 +273,6 @@ const Navbar: React.FC = () => {
             {isMenuOpen ? <IoClose size={24} /> : <CgMenuRightAlt size={24} />}
           </button>
 
-          {/* Desktop Navigation */}
           <motion.div
             initial={{ opacity: 1 }}
             animate={{ opacity: isSearchOpen ? 0.5 : 1 }}
@@ -124,7 +282,6 @@ const Navbar: React.FC = () => {
             <NavLinks />
           </motion.div>
 
-          {/* Search and Button */}
           <div className="hidden xl:flex items-center space-x-8">
             <div className="flex items-center gap-3">
               <AnimatePresence>
@@ -148,24 +305,17 @@ const Navbar: React.FC = () => {
                 <CiSearch size={32} />
               </button>
             </div>
-            <Link
-              onClick={() => {
-                window.scrollTo(0, 0);
-              }}
-              to={"/contact-us"}
-            >
+            <Link to="/contact-us" onClick={() => window.scrollTo(0, 0)}>
               <Button className="nav-button hidden sm:block" variant="primary">
                 Let's talk
               </Button>
             </Link>
-
           </div>
 
-          {/* Mobile Menu */}
           <AnimatePresence>
             {isMenuOpen && (
               <motion.div
-                className="fixed inset-0 bg-secondary-light2 z-40"
+                className="fixed inset-0 bg-white z-40"
                 initial={{ opacity: 0, y: "-100%" }}
                 animate={{ opacity: 1, y: 0 }}
                 exit={{ opacity: 0, y: "-100%" }}
@@ -174,17 +324,8 @@ const Navbar: React.FC = () => {
                 <div className="h-full flex flex-col pt-20">
                   <NavLinks isMobile onClick={toggleMenu} />
                   <div className="px-8 mt-8">
-                    <Link
-                      onClick={() => {
-                        window.scrollTo(0, 0);
-                      }}
-                      to={"/contact-us"}
-                    >
-                      <Button
-                        className="w-full"
-                        variant="primary"
-                        onClick={toggleMenu}
-                      >
+                    <Link to="/contact-us" onClick={() => window.scrollTo(0, 0)}>
+                      <Button className="w-full" variant="primary" onClick={toggleMenu}>
                         Let's talk
                       </Button>
                     </Link>
