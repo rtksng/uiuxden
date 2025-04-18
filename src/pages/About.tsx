df3f4cfb1c8adcceeb7cef9a0ab345a584c3bc41
tanisha <tanisha@seasia.in>
2025-04-18 15:17:09 +0530

    about-us-page-responsive

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -9,6 +9,7 @@ import aboutimg1 from "../assets/aboutimg1.png";
 import { Link } from "react-router-dom";
 import SVGAnimation from "../components/StrokeAnimation";
 import Tabs from "../components/Tabs";
+import HeaderBelowBannerVideo from "../assets/about/about-youtube-video.png";
 const About = () => {
 
   const tabData = [
@@ -34,24 +35,28 @@ const About = () => {
   return (
     <main className="aboutPage">
       <Navbar />
-      <section className="flex justify-center items-center h-[50vh] lg:h-[75vh] xl:h-screen pt-[100px]">
+      <section className="flex justify-center items-center h-auto pt-[100px] lg:pt-[150px] 2xl:pt-[200px] sm:mb-9 xl:mb-[150px]">
         <Container>
           <div className="grid grid-cols-12">
-            <div className="md:col-span-8 col-span-12 2xl:block flex items-center">
-              <h1 className="font-semibold text-[24px] sm:text-[38px] lg:text-[48px] xl:text-[70px] 2xl:text-[90px]">
+            <div className=" md:col-span-6 2xl:col-span-8 col-span-12 2xl:block flex items-center">
+              <h1 className="font-semibold text-[24px] sm:text-[38px] lg:text-[48px] 2xl:text-[70px] 2xl:text-[90px]">
                 Creating Exceptional Products, Shaping a Better Future
               </h1>
             </div>
-            <div className="md:col-span-4 col-span-10 self-center mt-8">
+            <div className=" md:col-span-6 2xl:col-span-4 col-span-10 self-center mt-8">
               <img src={futureimg} alt="image" className="mx-auto" />
             </div>
           </div>
+          <div className=" mt-9 lg:mt-28">
+            <img src={HeaderBelowBannerVideo} alt="image" className="mx-auto" />
+          </div>
         </Container>
       </section>
+
       <Container>
         <div className="md:gap-16 grid grid-cols-12 pt-[50px] sm:pt-0">
           <div className="lg:col-span-7 col-span-12 self-center">
-            <h3 className="font-semibold text-[24px] sm:text-[32px] lg:text-[40px]">
+            <h3 className="font-semibold text-[24px] sm:text-[32px] 2xl:text-[40px]">
               Driving creativity and innovation to shape a better future for
               ourselves, our partners, and communities around the world.
             </h3>
@@ -74,20 +79,21 @@ const About = () => {
               </p>
             </div>
           </div>
-          <div className="flex justify-center mt-4 md:mt-0 md:justify-end md:col-span-5 col-span-12 self-center">
+          <div className="flex justify-start mt-4 md:mt-0 lg:justify-end lg:col-span-5 col-span-12 self-center">
             <Link to="">
               <img src={aboutimg1} alt="image" />
             </Link>
           </div>
         </div>
       </Container>
-      <div className="mx-auto mt-9 sm:mt-[100px] lg:mt-[200px] max-w-[1792px] h-auto lg:h-[800px] text-white commitment">
+
+      <div className="mx-auto mt-9 sm:mt-[70px] lg:mt-[150px] 2xl:mt-[200px] max-w-[1792px] h-auto lg:h-[500px] 2xl:h-[800px] text-white commitment">
         <Container>
-          <div className="pt-[60px] lg:pt-[90px]">
-            <h4 className="font-semibold text-[24px] text-center text-white sm:text-[48px] xl:text-[60px]">
+          <div className="pt-[60px] lg:pt-[50px] 2xl:pt-[90px]">
+            <h4 className="font-semibold text-[24px] text-center text-white sm:text-[32px] lg:text-[40px] 2xl:text-[60px]">
               Our Commitment to
               <span className="animationStroke">
-              <SVGAnimation nameOfClass="svg-container aboutus" />
+                <SVGAnimation nameOfClass="svg-container excellence" />
                 Excellence
               </span>
             </h4>
@@ -97,10 +103,11 @@ const About = () => {
       </div>
       <Process />
       <Rating />
-      <div className="mt-10 md:mt-20 lg:mt-24">
+      <div className="mt-10 md:mt-20 lg:mt-24 2xl:mt-[150px] mb-20">
         <Container>
-          <h4 className="font-semibold text-[32px] sm:text-center text-black sm:text-[48px] xl:text-[60px] mb-5 sm:mb-9 lg:mb-36">
-            We love to make <span className="animationStroke"><SVGAnimation nameOfClass="svg-container" />things</span> easy
+          <h4 className="font-semibold sm:text-center text-black text-[24px] sm:text-[32px] lg:text-[40px] 2xl:text-[60px] mb-3 sm:mb-9 xl:mb-20 2xl:mb-36">
+            We love to make <span className="animationStroke">
+              <SVGAnimation nameOfClass="svg-container things" />things</span> easy
           </h4>
         </Container>
         <CaseStudyCarousel />
